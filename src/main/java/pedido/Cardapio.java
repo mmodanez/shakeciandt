9a117b8fd39fd30848174bf047c4769dfc659977
mmodanez <mmodanez@ciandt.com>
2022-07-15 07:36:29 -0300

    Cardapio rules implemented

--- src/main/java/pedido/Cardapio.java
+++ src/main/java/pedido/Cardapio.java
@@ -17,26 +17,47 @@ public class Cardapio {
     }
 
     public void adicionarIngrediente(Ingrediente ingrediente, Double preco) {
-        precos.put(ingrediente, preco);
+        if (preco <= 0.0) {
+            throw new IllegalArgumentException("Preco Invalido");
+        } else {
+            precos.put(ingrediente, preco);
+        }
     }
 
+    //Refactor - IllegalArgumentException on Preco Invalido
     public boolean atualizarIngrediente(Ingrediente ingrediente, Double preco) {
-        precos.replace(ingrediente, preco);
-        return true;
+        try {
+            if (preco <= 0.0) {
+                throw new IllegalArgumentException("Preco Invalido");
+            } else {
+                precos.replace(ingrediente, preco);
+                return true;
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ingrediente não existe no cardápio");
+        }
     }
 
     public boolean removerIngrediente(Ingrediente ingrediente) {
-        precos.remove(ingrediente);
-        return true;
+        try {
+            precos.remove(ingrediente);
+            return true;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ingrediente não existe no cardápio");
+        }
     }
 
     public Double buscarPreco(Ingrediente ingrediente) {
-        double preco = 0;
+        try {
+            double preco = 0;
 
-        for (int i = 0; i < precos.size(); i++) {
-            preco = precos.get(ingrediente);
+            for (int i = 0; i < precos.size(); i++) {
+                preco = precos.get(ingrediente);
+            }
+            return preco;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ingrediente não existe no cardápio");
         }
-        return preco;
     }
 
     @Override
